12873a5515ac2f1ac6d273c8d713be1c1b79dcee
arbitraryrw <nickcucakovic@gmail.com>
2020-12-05 00:58:48 +0000

    jsonhandler unit tests added for unstructed data happy path

--- internal/app/jsonhandler/jsonhandler_test.go
+++ internal/app/jsonhandler/jsonhandler_test.go
@@ -1,9 +1,44 @@
 package jsonhandler_test
 
 import (
+	"goencoderplayground/internal/app/jsonhandler"
+	"goencoderplayground/internal/pkg/utils"
 	"testing"
 )
 
 func TestUnmarshalJSONDataPositive(t *testing.T) {
 
+	testFileName := "basic.json"
+
+	testFilesSlice, err := utils.GetTestFile(testFileName)
+
+	if err != nil {
+		t.Errorf("GetTestFile() unable to find test file error: %q",
+			testFilesSlice)
+	}
+
+	if len(testFilesSlice) < 1 {
+		t.Errorf("GetTestFile() less than 1 match, expected 1 got %q",
+			len(testFilesSlice))
+	}
+
+	json, err := jsonhandler.UnmarshalJSONData(testFilesSlice[0])
+
+	if err != nil {
+		t.Errorf("UnmarshalJSONData() unmarshal file %q, got error: %q",
+			testFileName,
+			testFilesSlice)
+	}
+
+	widget := json.(map[string]interface{})["widget"]
+	widgetImage := widget.(map[string]interface{})["image"]
+	widgetImageName := widgetImage.(map[string]interface{})["name"]
+
+	expectedValue := "sun1"
+
+	if widgetImageName != expectedValue {
+		t.Errorf("Unable to find expected value %q in json response %q",
+			expectedValue,
+			widgetImageName)
+	}
 }
