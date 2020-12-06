7000d0a77819c272e85bd5f6709ca6eec3f33fcd
arbitraryrw <nickcucakovic@gmail.com>
2020-12-06 11:37:51 +0000

    xmlhandler test included basic positive test

--- internal/app/xmlhandler/xmlhandler_test.go
+++ internal/app/xmlhandler/xmlhandler_test.go
@@ -1,1 +1,32 @@
 package xmlhandler_test
+
+import (
+	"goencoderplayground/internal/app/xmlhandler"
+	"goencoderplayground/internal/pkg/utils"
+	"testing"
+)
+
+func TestUnmarshalXMLDataPositive(t *testing.T) {
+	testFileName := "basic.xml"
+
+	testFilesSlice, err := utils.GetTestFile(testFileName)
+
+	if err != nil {
+		t.Errorf("GetTestFile() unable to find test file error: %q",
+			testFilesSlice)
+	}
+
+	if len(testFilesSlice) < 1 {
+		t.Errorf("GetTestFile() less than 1 match, expected 1 got %q",
+			len(testFilesSlice))
+	}
+
+	_, err := xmlhandler.UnmarshalXMLData(testFilesSlice[0])
+
+	if err != nil {
+		t.Errorf("UnmarshalXMLData() unmarshal file %q, got error: %q",
+			testFileName,
+			testFilesSlice)
+	}
+
+}
