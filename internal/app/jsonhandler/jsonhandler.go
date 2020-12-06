43f5c16dedd8a339de33c15dd91e235539389850
arbitraryrw <nickcucakovic@gmail.com>
2020-12-06 12:04:07 +0000

    jsonhandler creating go structure for basic.json test file

--- internal/app/jsonhandler/jsonhandler.go
+++ internal/app/jsonhandler/jsonhandler.go
@@ -6,6 +6,37 @@ import (
 	"goencoderplayground/internal/pkg/utils"
 )
 
+// BasicStructuredJSON test data structure for basic.json
+// Generated using https://transform.tools/json-to-go
+type BasicStructuredJSON struct {
+	Widget struct {
+		Debug  string `json:"debug"`
+		Window struct {
+			Title  string `json:"title"`
+			Name   string `json:"name"`
+			Width  int    `json:"width"`
+			Height int    `json:"height"`
+		} `json:"window"`
+		Image struct {
+			Src       string `json:"src"`
+			Name      string `json:"name"`
+			HOffset   int    `json:"hOffset"`
+			VOffset   int    `json:"vOffset"`
+			Alignment string `json:"alignment"`
+		} `json:"image"`
+		Text struct {
+			Data      string `json:"data"`
+			Size      int    `json:"size"`
+			Style     string `json:"style"`
+			Name      string `json:"name"`
+			HOffset   int    `json:"hOffset"`
+			VOffset   int    `json:"vOffset"`
+			Alignment string `json:"alignment"`
+			OnMouseUp string `json:"onMouseUp"`
+		} `json:"text"`
+	} `json:"widget"`
+}
+
 //ref: https://golang.org/pkg/encoding/json/
 
 //UnmarshalJSONData converts bytevalue to map
